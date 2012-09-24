66208815a18fef3824ee77b6a88a9f2b60a3ffa6
Reuben Doetsch <reuben.doetsch@gmail.com>
2012-09-24 12:03:26 -0400

    Changed

--- src/main/scala/ReleaseExtra.scala
+++ src/main/scala/ReleaseExtra.scala
@@ -110,7 +110,7 @@ object ReleaseStateTransformations {
   lazy val commitNextVersion: ReleaseStep = ReleaseStep(commitVersion("Bump to %s"))
   private[sbtrelease] def commitVersion(msgPattern: String) = { st: State =>
     val v = st.extract.get(version in ThisBuild)
-    val v2 = st.extract.get(version)
+    val v2 = st.extract.get(releaseVersion)
 
     val info = st.extract.currentProject
     val path = info.base + "/version.sbt"
@@ -119,7 +119,7 @@ object ReleaseStateTransformations {
     val status = (Git.status !!) trim
 
     if (status.nonEmpty) {
-      Git.commit(msgPattern format version) ! st.log
+      Git.commit(msgPattern format v2) ! st.log
     } else {
       // nothing to commit. this happens if the version.sbt file hasn't changed.
     }
